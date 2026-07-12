d3bb8a92f39f60daff45c07753b2d2938da5d180
Daria-99 <dashakrilova.22@yandex.ru>
2026-07-12 16:38:21 +0500

    refactor(components): migrate styles to css-modules

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import "./Header.scss";
+import classes from "./Header.module.scss";
 
 import recipeIcon from "../../assets/icons/salad-icon.png";
 import plannerIcon from "../../assets/icons/planner-icon.png";
@@ -6,18 +6,20 @@ import messageIcon from "../../assets/icons/message-icon.png";
 
 const Header = () => {
   return (
-    <div className="header">
-      <ul className="navigation">
-        <li className="navigation__item navigation__item--active">
-          <img className="navigation__icon" src={recipeIcon} />
+    <div className={classes["header"]}>
+      <ul className={classes["navigation"]}>
+        <li
+          className={`${classes["navigation__item"]} ${classes["navigation__item--active"]}`}
+        >
+          <img className={classes["navigation__icon"]} src={recipeIcon} />
           Рецепты
         </li>
-        <li className="navigation__item ">
-          <img className="navigation__icon" src={plannerIcon} />
+        <li className={classes["navigation__item"]}>
+          <img className={classes["navigation__icon"]} src={plannerIcon} />
           Планировщик
         </li>
-        <li className="navigation__item">
-          <img className="navigation__icon" src={messageIcon} />
+        <li className={classes["navigation__item"]}>
+          <img className={classes["navigation__icon"]} src={messageIcon} />
           Связь
         </li>
       </ul>
