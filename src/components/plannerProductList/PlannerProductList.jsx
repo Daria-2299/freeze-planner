413ff18eeebf5809d7b1715d837fa1b817c94989
Daria-99 <dashakrilova.22@yandex.ru>
2026-08-15 12:48:27 +0500

    feat: add copy and download functionality for product list

--- src/components/plannerProductList/PlannerProductList.jsx
+++ src/components/plannerProductList/PlannerProductList.jsx
@@ -1,14 +1,62 @@
 import classes from "./plannerProductList.module.scss";
 
 import cartIcon from "../../assets/icons/shopping-cart-icon.png";
+import copyIcon from "../../assets/icons/copy-icon.png";
+import downloadIcon from "../../assets/icons/download-icon.png";
 
 const PlannerProductList = ({ ingredients }) => {
+  const handleCopyList = () => {
+    const listText = ingredients
+      .map(
+        (ingredient, index) =>
+          `${index + 1}. ${ingredient.title} ${ingredient.weight} ${ingredient.measure}`,
+      )
+      .join("\n");
+
+    navigator.clipboard.writeText(listText);
+  };
+
+  const handleSaveAsFile = () => {
+    const listText = ingredients
+      .map(
+        (ingredient, index) =>
+          `${index + 1}. ${ingredient.title} ${ingredient.weight} ${ingredient.measure}`,
+      )
+      .join("\n");
+    const blob = new Blob([listText], { type: "text/plain;charset=utf-8" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = `список_продуктов_${new Date().toLocaleDateString()}.txt`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(link.href);
+  };
+
   return (
     <div className={classes["products-block"]}>
-      <h3 className={classes["products-block__title"]}>
-        <img src={cartIcon} className={classes["products-block__icon"]} />
-        Список продуктов
-      </h3>
+      <div className={classes["products-block__top"]}>
+        <h3 className={classes["products-block__title"]}>
+          <img src={cartIcon} className={classes["products-block__icon"]} />
+          Список продуктов
+        </h3>
+        {ingredients.length > 0 && (
+          <div className={classes["buttons"]}>
+            <button
+              onClick={handleCopyList}
+              className={classes["buttons__item"]}
+            >
+              <img src={copyIcon} className={classes["buttons__icon"]} />
+            </button>
+            <button
+              onClick={handleSaveAsFile}
+              className={classes["buttons__item"]}
+            >
+              <img src={downloadIcon} className={classes["buttons__icon"]} />
+            </button>
+          </div>
+        )}
+      </div>
       {ingredients.length > 0 ? (
         <div className={classes["products-block__table"]}>
           <div className={classes["headlines"]}>
